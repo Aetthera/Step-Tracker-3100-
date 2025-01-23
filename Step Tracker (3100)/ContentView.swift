a8dce658721e4c86da45e45e7d8e18f1bb7732b5
Alena <alenabelovadmvn@gmail.com>
2025-01-23 09:12:25 -0500

    Initial Commit

---
+++ Step Tracker (3100)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Step Tracker (3100)
+//
+//  Created by Alena Belova  on 2025-01-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Step Tracker (3100)/Step_Tracker__3100_App.swift
@@ -0,0 +1,17 @@
+//
+//  Step_Tracker__3100_App.swift
+//  Step Tracker (3100)
+//
+//  Created by Alena Belova  on 2025-01-23.
+//
+
+import SwiftUI
+
+@main
+struct Step_Tracker__3100_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
